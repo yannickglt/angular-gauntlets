0466efeb2218c039572653a560ce2e793ce44857
Yannick Galatol <yannick.galatol@hotmail.fr>
2018-02-16 00:12:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Angular Gauntlets
+Add shortcuts to easily debug your Angular 2+ application
+
+![logo](https://user-images.githubusercontent.com/1006426/36344659-faf8a79e-141d-11e8-8ae1-a4a51e26af80.png)
+
+Angular Gauntlets helps you getting your hands dirty. 
+This chrome extension makes the components and providers of your Angular application accessible on click on an element in the Chrome DevTools elements panel.
+
+## Install
+Download the extension from the Chrome Web Store:
+
+[https://chrome.google.com/webstore/detail/angular-gauntlets/cpinnkgbfohodioinipjdhehnihbgeph](https://chrome.google.com/webstore/detail/angular-gauntlets/cpinnkgbfohodioinipjdhehnihbgeph)
+
+## How to use
+Gauntlets is light, based on `ng.probe`, loads instantly and does not need to switch to another tab of the Chrome DevTools. 
+
+Just click on a DOM element in the DevTools elements panel, type one of the following keywords in the console and here we go!
+- `$component`: click on a DOM element referring to an Angular component to access its instance and playing with it.
+- `$context`: display a DOM element context, can be its parent Angular component, a `ngFor` row, etc.
+- `$scope`: equals `$component` onto Angular components or `$context` onto child elements. The best alternative of [AngularJS Batarang](https://github.com/angular/batarang) for Angular.
+- `$providers`: get the list of the all the providers instantiated in your Angular app and play with them.
+- `$detectChanges()`: triggers Angular change detection to get your changes apply.
+
+**Little extra**: you can get a preview of all these info in the `Angular` tab of your DevTools elements panel. Switching between elements will refresh the data immediately.
+
+## Demo
+### Playing with components 
+![component](https://user-images.githubusercontent.com/1006426/36344628-b894ca72-141d-11e8-8fb8-92f196160ec2.gif)
+
+### Playing with providers
+![providers](https://user-images.githubusercontent.com/1006426/36344666-206bf1ac-141e-11e8-8a66-7d93dec41067.gif)
+
+### State preview
+![panel](https://user-images.githubusercontent.com/1006426/36344674-4ef7daa4-141e-11e8-88de-9c1edfa9abd7.png)
+
+## License 
+MIT
---
+++ devtoolsBackground.js
@@ -0,0 +1,85 @@
+const elementsPanel = chrome && chrome.devtools && chrome.devtools.panels && chrome.devtools.panels.elements;
+if (elementsPanel) {
+  elementsPanel.createSidebarPane('Angular', function onSidebarCreated(sidebar) {
+    elementsPanel.onSelectionChanged.addListener(function updateElementProperties() {
+      sidebar.setExpression('(' + getPanelContents.toString() + ')()');
+    });
+  });
+}
+
+function getPanelContents() {
+  let debugElement = null;
+  let componentInstance = null;
+  let context = null;
+  let providers = null;
+  let detectChanges = () => null;
+
+  const ng = window.ng;
+  if ($0 && ng) {
+    debugElement = ng.probe($0) || null;
+    if (debugElement) {
+      componentInstance = debugElement.componentInstance;
+      context = debugElement.context.$implicit ? debugElement.context.$implicit : debugElement.context;
+      providers = getProviders(debugElement, ng) || getLegacyProviders(debugElement, ng);
+      detectChanges = () => debugElement._debugInfo._view.changeDetectorRef.detectChanges();
+    }
+  }
+
+  exportToWindow(debugElement, componentInstance, context, providers, detectChanges);
+
+  return {
+    debugElement,
+    componentInstance,
+    context,
+    providers
+  };
+
+  function exportToWindow(debugElement, componentInstance, context, providers, detectChanges) {
+    window.$debugElement = debugElement;
+    window.$component = componentInstance;
+    window.$context = context;
+    window.$providers = providers;
+    window.$scope = componentInstance || context; // AngularJS Batarang compatibility
+    window.$detectChanges = detectChanges;
+  }
+
+  // Angular 4+
+  function getProviders(debugElement, ng) {
+    const injector = debugElement.injector.get(ng.coreTokens.ApplicationRef)._injector;
+    const providers = {};
+    if (injector && injector._providers) {
+      injector._providers
+        .forEach(provider => {
+          const key = provider && provider.constructor ? provider.constructor.name : String(provider);
+          providers[lowerFirst(key)] = provider;
+        });
+      return providers;
+    } else {
+      return null;
+    }
+  }
+
+  // Angular 2-3
+  function getLegacyProviders(debugElement, ng) {
+    const injector = debugElement.injector.get(ng.coreTokens.ApplicationRef)._injector;
+    if (injector) {
+      const providers = {};
+      for (const key in injector) {
+        const res = /_([A-Z]+[^_]*)_+\d+/.exec(key);
+        if (res && res[1]) {
+          const providerName = res[1];
+          providers[lowerFirst(providerName)] = injector[key];
+        }
+      }
+      return providers;
+    } else {
+      return null;
+    }
+  }
+
+  function lowerFirst(str) {
+    str = String(str);
+    return str.charAt(0).toLowerCase() + str.slice(1);
+  }
+
+}
